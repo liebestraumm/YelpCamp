97496721b7bd075757a2a49645ceed05b6893ac7
Carlos <carlosarturo.agelvisvergara@connect.qut.edu.au>
2020-01-04 13:27:07 +1000

    Adding data persistence with MongoDB and Mongoose

--- v1/app.js
+++ v1/app.js
@@ -1,17 +1,20 @@
-var express = require("express");
-var app = express();
-var bodyP = require("body-parser");
+var express 	= require("express"),
+	app 		= express(),
+  	bodyP 		= require("body-parser"),
+  	mongoose	= require("mongoose");
 
+mongoose.connect("mongodb://localhost:27017/yelp_camp",  {useUnifiedTopology: true, useNewUrlParser: true});
 app.use(bodyP.urlencoded({extended: true}));
 app.use(express.static("public"));
 app.use(express.static("images"));
 app.set("view engine", "ejs");
 
-var campImages = [
-	{name: "Avila", image: "Avila.png"},
-	{name: "La Sabanita", image: "La_Sabanita.png"},
-	{name: "Gesundbrunnen", image: "Wansee.png"}
-];
+var campgroundSchema = new mongoose.Schema({
+		name: String,
+		image: String
+});
+
+var Campground = mongoose.model("Campground", campgroundSchema);
 
 app.get("/", function(req, res){
 
@@ -19,8 +22,14 @@ app.get("/", function(req, res){
 });
 
 app.get("/campgrounds", function(req, res){
-
-	res.render("campgrounds", {campImages:campImages});
+	Campground.find({}, function(error, campgrounds){
+		if(error){
+			console.log(error);
+		}
+		else{
+			res.render("campgrounds", {campgrounds:campgrounds})
+		}
+	});	
 });
 
 app.post("/campgrounds", function(req, res){
@@ -28,14 +37,21 @@ app.post("/campgrounds", function(req, res){
 	var name = req.body.camp;
 	var image = req.body.image;
 
-	campImages.push({name:name, image:image});
+	var campground = new Campground({name: name, image: image});
+
+	campground.save(function(error, campground){
+		if(error)
+		{
+			console.log("Error Saving: " + error);
+		}
+	});
 
 	res.redirect("/campgrounds");
 });
 
 app.get("/campgrounds/new", function(req, res){
 
-	res.render("new", {campImages:campImages});
+	res.render("new");
 });
 
 
