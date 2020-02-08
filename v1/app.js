9f4d66248fcfc153c882f4d8719230c3c7aacda0
Carlos <carlosarturo.agelvisvergara@connect.qut.edu.au>
2020-02-08 21:36:26 +1000

    Added comments and necessary style. Renamed app.cs file to main.css and also created new directories for views for better organization

--- v1/app.js
+++ v1/app.js
@@ -2,23 +2,20 @@ var express 	= require("express"),
 	app 		= express(),
   	bodyP 		= require("body-parser"),
   	mongoose	= require("mongoose");
+    Campground  = require("./models/campground");
+    Comment     = require("./models/comment")
+    seedDB      = require("./seed")
 
+//seedDB();
 mongoose.connect("mongodb://localhost:27017/yelp_camp",  {useUnifiedTopology: true, useNewUrlParser: true});
 app.use(bodyP.urlencoded({extended: true}));
-app.use(express.static("public"));
+app.use(express.static(__dirname+"/public"));
 app.use(express.static("images"));
 app.set("view engine", "ejs");
 
-var campgroundSchema = new mongoose.Schema({
-		name: String,
-		image: String
-});
-
-var Campground = mongoose.model("Campground", campgroundSchema);
-
 app.get("/", function(req, res){
 
-	res.render("home");
+	res.render("./home");
 });
 
 app.get("/campgrounds", function(req, res){
@@ -27,7 +24,7 @@ app.get("/campgrounds", function(req, res){
 			console.log(error);
 		}
 		else{
-			res.render("campgrounds", {campgrounds:campgrounds})
+			res.render("./campgrounds/index", {campgrounds:campgrounds})
 		}
 	});	
 });
@@ -36,8 +33,8 @@ app.post("/campgrounds", function(req, res){
 
 	var name = req.body.camp;
 	var image = req.body.image;
-
-	var campground = new Campground({name: name, image: image});
+	var description = req.body.description;
+	var campground = new Campground({name: name, image: image, description: description});
 
 	campground.save(function(error, campground){
 		if(error)
@@ -51,9 +48,57 @@ app.post("/campgrounds", function(req, res){
 
 app.get("/campgrounds/new", function(req, res){
 
-	res.render("new");
+	res.render("./campgrounds/new");
 });
 
+app.get("/campgrounds/:id", function(req, res){
+    //find the campground with provided ID
+		Campground.findById(req.params.id).populate("comments").exec(function(error, found){
+			if(error)
+				console.log(error);
+			else{
+                console.log(found);
+				res.render("./campgrounds/show", {campground: found});
+			}
+		});	
+});
+
+
+//===================================
+//COMMENTS ROUTES
+//===================================
+app.get("/campgrounds/:id/comments/new", function(req,res){
+    //find campground by id
+    Campground.findById(req.params.id, function(err, campground){
+        if(err){
+            console.log(err);
+        }
+        else{
+            
+            res.render("./comments/new", {campground: campground})
+        }
+    } )
+});
+
+app.post("/campgrounds/:id/comments", function(req, res){
+    //lookup campground using ID
+    Campground.findById(req.params.id, function(err, campground){
+        if(err)
+            console.log(err);
+        else{
+           Comment.create(req.body.comments, function(err, comments){
+               if(err)
+                   console.log(err)
+               else
+                   {
+                    campground.comments.push(comments);
+                    campground.save();
+                    res.redirect("/campgrounds/" + campground._id)
+                   }
+           });
+        }
+    });
+})
 
 app.listen(3000, function(){
 
---
+++ v1/seed.js
@@ -0,0 +1,60 @@
+var express 	= require("express");
+var app = express();
+
+app.use(express.static("images"))
+var mongoose = require("mongoose");
+var Campground = require("./models/campground");
+var Comment = require("./models/comment")
+
+var data = [
+    {
+        name: "El Avila",
+        image: "/avila.png",
+        description: "El avila magica"
+    },
+    {
+        name: "La Sabanita",
+        image: "/La_Sabanita.png",
+        description: "La sabana de Bocono"
+    },
+    {
+        name: "Wansee Lake",
+        image: "/Wansee.png",
+        description: "I Love Berlin"
+    }
+]
+function seedDB(){
+    //Remove all campgrounds
+    Campground.remove({}, function(err){
+    if(err)
+        console.log(err);
+    console.log("removed campgrounds");
+    //add campgrounds
+    data.forEach(function(seed){
+        Campground.create(seed, function(err, campground){
+            if(err)
+                console.log(err);
+            else{
+                console.log("added a compground");
+                //Create a comment
+                Comment.create({
+                    text: "This place is great",
+                    author: "Carlos"
+                }, function(err, comment){
+                    if(err)
+                        console.log(err)
+                    else 
+                    {
+                    campground.comments.push(comment);
+                    campground.save();
+                    console.log("Comment created")
+                    }
+                });
+            }
+        });
+    })
+});
+    
+}
+
+module.exports = seedDB;
